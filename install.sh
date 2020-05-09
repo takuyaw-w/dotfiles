36d9867e01f20bd4411045e1e14947fbfbcef582
takuyaw-w <takuya.jamtb@gmail.com>
2020-05-09 21:38:50 +0900

    echo入れた

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!bin/env bash
 
 # 初期設定
 # wget -qO- https://raw.githubusercontent.com/takuyaw-w/dotfiles/master/install.sh | sh
@@ -43,56 +43,82 @@ fi
 # アプリケーションのインストール
 
 # 各PPA
+echo add-apt-repository lazygit
 sudo add-apt-repository -y ppa:lazygit-team/release
+echo gerardpuig
 sudo add-apt-repository -y ppa:gerardpuig/ppa
+echo git-core
 sudo add-apt-repository -y ppa:git-core/ppa
 
 # libreoffice
+echo install libreoffice
 sudo snap install libreoffice
 # gimp
+echo install gimp
 sudo snap install gimp
 # thunderbird
+echo install thunderbird
 sudo snap install thunderbird
 # go language
+echo golang
 sudo snap install go --classic
 # Hugo
+echo hugo
 sudo snap install hugo
 
 # vim
+echo install vim
 sudo apt install -y vim
 # git
+echo install git
 sudo apt install -y git
 # curl
+echo install curl
 sudo apt install -y curl
 # tree
+echo install tree
 sudo apt install -y tree
 # Rust
+echo install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # deno
+echo install deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
 # git-lfs
+echo install git-lfs
 sudo apt install -y git-lfs
 # git-flow
+echo install git-flow
 sudo apt install -y git-flow
 # lazygit
+echo install lazygit
 sudo apt install -y lazygit
 # gnome-tweaks
+echo install gnome-tweaks
 sudo apt install -y gnome-tweaks
 # zsh
+echo install zsh
 sudo apt install -y zsh
 # byobu
+echo install byobu
 sudo apt install -y byobu
 # ghq
+echo install ghq
 go get github.com/x-motemen/ghq
 # peco
+echo install peco
 sudo apt install -y peco
 # build-essential
+echo install build-essential
 sudo apt install -y build-essential
 # virtualenv
+echo install virtualenv
 sudo apt install -y virtualenv
 # virtualenvwrapper
+echo install virtualenvwrapper
 sudo apt install -y virtualenvwrapper
 # nvm
+echo install nvm
 git clone https://github.com/nvm-sh/nvm.git .nvm
 cd ~/.nvm
 git checkout v0.35.3
@@ -103,9 +129,11 @@ source nvm.sh
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
 # nodejs
+echo install nodejs
 nvm install --lts --latest-npm
 nvm alias default lts/*
 # powerline
+echo install fonts
 sudo apt install -y powerline
 # fonts
 sudo apt install -y fonts-powerline
@@ -114,9 +142,11 @@ sudo apt install -y fonts-noto
 sudo apt install -y fonts-ricty-diminished
 
 # oh-my-zsh clone
+echo clone oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # dotfiles clone
+echo clone dotfile
 git clone https://github.com/takuyaw-w/dotfiles.git
 # リポジトリの中の.zshrcなどのシンボリックリンクを貼る
 ln -sfv ~/dotfiles/.zshrc ~/.zshrc
