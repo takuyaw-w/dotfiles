54c55c3a0809c3adc661ece05c2fc6239fefaac8
takuyaw-w <takuya.jamtb@gmail.com>
2020-05-28 20:17:03 +0900

    fix

--- install.sh
+++ install.sh
@@ -79,6 +79,7 @@ echo visual-studio-code repository
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo rm -rf packages.microsoft.gpg
 
 sudo apt update
 sudo apt -y upgrade
