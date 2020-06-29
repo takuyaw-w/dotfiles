aa4c939aece3833e0d0a9b89f86695db4b5475d9
takuyaw-w <takuya.jamtb@gmail.com>
2020-06-30 00:20:43 +0900

    update

--- .zsh/rc/pluginlist.zsh
+++ .zsh/rc/pluginlist.zsh
@@ -1,26 +1,26 @@
 # Setup zinit
-if [ -z "$ZPLG_HOME" ]; then
-    ZPLG_HOME="${ZHOMEDIR:-$HOME}/zinit"
-fi
+# if [ -z "$ZPLG_HOME" ]; then
+#     ZPLG_HOME="${ZHOMEDIR:-$HOME}/zinit"
+# fi
 
-if ! test -d "$ZPLG_HOME"; then
-    mkdir "$ZPLG_HOME"
-    chmod g-rwX "$ZPLG_HOME"
-    git clone --depth 10 https://github.com/zdharma/zinit.git ${ZPLG_HOME}/bin
-fi
+# if ! test -d "$ZPLG_HOME"; then
+#     mkdir "$ZPLG_HOME"
+#     chmod g-rwX "$ZPLG_HOME"
+#     git clone --depth 10 https://github.com/zdharma/zinit.git ${ZPLG_HOME}/bin
+# fi
 
-typeset -gAH ZPLGM
-ZPLGM[HOME_DIR]="${ZPLG_HOME}"
-source "$ZPLG_HOME/bin/zinit.zsh"
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
+# typeset -gAH ZPLGM
+# ZPLGM[HOME_DIR]="${ZPLG_HOME}"
+# source "$ZPLG_HOME/bin/zinit.zsh"
+# autoload -Uz _zinit
+# (( ${+_comps} )) && _comps[zinit]=_zinit
 
-#==============================================================#
-## Plugin load                                                ##
-#==============================================================#
+# #==============================================================#
+# ## Plugin load                                                ##
+# #==============================================================#
 
 
-#==============================================================#
-# local plugins
-#==============================================================#
-[ -f "$HOME/.zshrc.plugin.local" ] && source "$HOME/.zshrc.plugin.local"
+# #==============================================================#
+# # local plugins
+# #==============================================================#
+# [ -f "$HOME/.zshrc.plugin.local" ] && source "$HOME/.zshrc.plugin.local"
