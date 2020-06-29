00e4ea846e0ee992ff79ccf94e9e81dcf82c3514
takuyaw-w <takuya.jamtb@gmail.com>
2020-06-29 23:14:25 +0900

    update

--- .zsh/rc/bindkey.zsh
+++ .zsh/rc/bindkey.zsh
@@ -1,1 +1,12 @@
 # Key Bindings
+
+function ghq-fzf() {
+  local src=$(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
+zle -N ghq-fzf
+bindkey '^]' ghq-fzf
