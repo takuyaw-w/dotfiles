bad58a640be357f3bbd94074c4f653052f83fca5
takuyaw-w <takuya.jamtb@gmail.com>
2020-08-15 05:44:12 +0900

    update

--- .bin/lib/input-method.sh
+++ .bin/lib/input-method.sh
@@ -4,7 +4,7 @@ source $(dirname "${BASH_SOURCE[0]:-$0}")/utilfuncs.sh
 
 info_message "Install and configure the packages required for the input method."
 
-sudo pacman -S --noconfirm --needed fcitx-im fcitx-mozc
+sudo pacman -S --noconfirm --needed fcitx-im fcitx-mozc fcitx-configtool
 
 cat << EOF >> ~/.xprofile
 export LANG="ja_JP.UTF-8"
