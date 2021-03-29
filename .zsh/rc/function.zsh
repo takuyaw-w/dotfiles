7bab36babdc7498c211597a4ed74305f275dadf2
takuyaw-w <takuya.jamtb@gmail.com>
2021-03-29 22:36:49 +0900

    update

--- .zsh/rc/function.zsh
+++ .zsh/rc/function.zsh
@@ -1,13 +1,1 @@
 # functions
-
-function get_max_brightness() {
-  less "/sys/class/backlight/intel_backlight/max_brightness"
-}
-
-funtion get_brightness() {
-  less "/sys/class/backlight/intel_backlight/brightness"
-}
-
-funtion set_brightness() {
-  sudo tee /sys/class/backlight/intel_backlight/brightness <<< $1
-}
