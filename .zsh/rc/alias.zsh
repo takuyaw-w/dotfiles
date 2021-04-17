68e5968b2ab293406412fe46f44e1de059b1f008
takuyaw-w <takuya.jamtb@gmail.com>
2021-04-17 23:06:16 +0900

    update

--- .zsh/rc/alias.zsh
+++ .zsh/rc/alias.zsh
@@ -1,6 +1,6 @@
 # Aliases
 
-alias open=xdg-open
+alias open="xdg-open"
 
 # ls command
 if command "exa" -v > /dev/null 2>&1; then
@@ -16,26 +16,33 @@ if command "bat" -v > /dev/null 2>&1; then
   alias cat="bat"
 fi
 
-alias grep="grep --color=auto"
-alias egrep="egrep --color=auto"
+# grep command
+if command "rg" -v > /dev/null 2>&1; then
+  alias grep="rg"
+else
+  alias grep="grep --color=auto"
+  alias egrep="egrep --color=auto"
+fi
 
-ex() {
-  if [ -f $1 ]; then
-    case $1 in
-    *.tar.bz2) tar xjf $1 ;;
-    *.tar.gz) tar xzf $1 ;;
-    *.bz2) bunzip2 $1 ;;
-    *.rar) unrar x $1 ;;
-    *.gz) gunzip $1 ;;
-    *.tar) tar xf $1 ;;
-    *.tbz2) tar xjf $1 ;;
-    *.tgz) tar xzf $1 ;;
-    *.zip) unzip $1 ;;
-    *.Z) uncompress $1 ;;
-    *.7z) 7z x $1 ;;
-    *) echo "'$1' cannot be extracted via ex()" ;;
-    esac
-  else
-    echo "'$1' is not a valid file"
-  fi
+function extract() {
+  case $1 in
+    *.tar.gz|*.tgz) tar xzvf "$1";;
+    *.tar.xz) tar Jxvf "$1";;
+    *.zip) unzip "$1";;
+    *.lzh) lha e "$1";;
+    *.tar.bz2|*.tbz) tar xjvf "$1";;
+    *.tar.Z) tar zxvf "$1";;
+    *.gz) gzip -d "$1";;
+    *.bz2) bzip2 -dc "$1";;
+    *.Z) uncompress "$1";;
+    *.tar) tar xvf "$1";;
+    *.arj) unarj "$1";;
+  esac
 }
+alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}="extract"
+
+# ghq command
+function ghq_create() {
+  ghq create $1
+}
+alias ghqc="ghq_create"
