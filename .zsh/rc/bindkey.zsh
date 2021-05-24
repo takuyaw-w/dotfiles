5205bdf911e490f87d192483308f0dd4ca33b23f
takuyaw-w <takuya.jamtb@gmail.com>
2021-05-25 00:57:12 +0900

    update

--- .zsh/rc/bindkey.zsh
+++ .zsh/rc/bindkey.zsh
@@ -1,15 +1,4 @@
 # Key Bindings
 
-function ghq-fzf() {
-  local selected_dir=$(ghq list | fzf --query="$LBUFFER" --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
-
-  if [ -n "$selected_dir" ]; then
-    BUFFER="cd $(ghq root)/${selected_dir}"
-    zle accept-line
-  fi
-
-  zle reset-prompt
-}
-
 zle -N ghq-fzf
 bindkey "^]" ghq-fzf
--- .zsh/rc/function.zsh
+++ .zsh/rc/function.zsh
@@ -1,1 +1,11 @@
 # functions
+function ghq-fzf() {
+  local selected_dir=$(ghq list | fzf --query="$LBUFFER" --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd $(ghq root)/${selected_dir}"
+    zle accept-line
+  fi
+
+  zle reset-prompt
+}
