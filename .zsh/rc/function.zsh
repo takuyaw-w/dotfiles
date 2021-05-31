27bc10deea3469b6a9308c12ceeec8545f1585d9
takuyaw-w <takuya.jamtb@gmail.com>
2021-06-01 02:11:12 +0900

    added functions

--- .zsh/rc/function.zsh
+++ .zsh/rc/function.zsh
@@ -9,3 +9,46 @@ function ghq-fzf() {
 
   zle reset-prompt
 }
+
+function ghq-update() {
+  ghq list | ghq get --update --parallel
+}
+
+function ghq-export() {
+  ghq list > repolist.txt
+}
+
+function ghq-import() {
+  cat $1 | ghq get --parallel
+}
+
+function tmux-ide() {
+  tmux
+  if [ "$#" -eq 0 ]; then
+    tmux split-window -v
+    tmux split-window -h
+    tmux resize-pane -D 15
+    tmux select-pane -t 1
+  else
+    case $1 in
+    1)
+      tmux split-window -v
+      tmux resize-pane -D 15
+      tmux select-pane -D
+      clear
+      ;;
+    2)
+      tmux split-window -h
+      tmux split-window -v
+      tmux resize-pane -D 15
+      tmux select-pane -t 1
+      tmux split-window -v
+      tmux select-pane -t 1
+      clear
+      ;;
+    *)
+      echo [ERROR] "$1" は設定されていない引数です。
+      ;;
+    esac
+  fi
+}
