2d61ae01b1eff9d3fc83d7f6164b3b99a140626b
takuyaw-w <takuya.jamtb@gmail.com>
2022-09-12 23:16:01 +0900

    wip

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,15 +3,16 @@ local wezterm = require("wezterm")
 local config = {
     check_for_updates = false,
     use_ime = true,
-    color_scheme = "Afterglow (Gogh)",
+    color_scheme = "nordfox",
     font = wezterm.font_with_fallback {
         'Source Han Code JP',
         'ricty diminished',
         'Cascadia Code', -- for windows
         'Cascadia Mono' -- for windows
     },
-    font_size = 14.0,
+    font_size = 12.0,
     adjust_window_size_when_changing_font_size = false,
+    tab_bar_at_bottom = true,
 }
 
 return config
