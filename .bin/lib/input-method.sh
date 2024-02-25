a55e15b667fe1e8e24068c5e2ea4320e3204b140
takuyaw-w <takuya.jamtb@gmail.com>
2024-02-25 21:42:32 +0900

    update

--- .bin/lib/input-method.sh
+++ .bin/lib/input-method.sh
@@ -6,14 +6,4 @@ info_message "Install and configure the packages required for the input method."
 
 sudo pacman -S --noconfirm --needed fcitx5-im fcitx5-mozc
 
-cat << EOF >> ~/.xprofile
-export LANG="ja_JP.UTF-8"
-export XMODIFIERS="@im=fcitx"
-export XMODIFIER="@im=fcitx"
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export DefaultIMModule=fcitx
-export GLFW_IM_MODULE=ibus
-EOF
-
 complete_message "Package install and configuration is done."
