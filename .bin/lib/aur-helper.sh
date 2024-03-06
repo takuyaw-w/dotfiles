c33fd4c58b1bc4ad3c8cd624484ffa3d54e71535
takuyaw-w <takuya.jamtb@gmail.com>
2024-03-07 03:15:37 +0900

    update

--- .bin/lib/aur-helper.sh
+++ .bin/lib/aur-helper.sh
@@ -6,10 +6,10 @@ info_message "Start install AUR helper"
 
 if ! builtin command -v paru >/dev/null 2>&1; then
   if [ ! -d /tmp/paru ]; then
-    (cd /tmp && git clone https://aur.archlinux.org/paru.git)
+    (cd /tmp && git clone https://aur.archlinux.org/paru-bin.git)
   fi
   sudo pacman -S --noconfirm --needed base-devel 
-  (cd /tmp/paru && makepkg -sif --noconfirm && paru -Syy)
+  (cd /tmp/paru-bin && makepkg -sif --noconfirm && paru -Syy)
 fi
 
 complete_message "Install AUR helper done."
